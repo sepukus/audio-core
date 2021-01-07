a7d4844cfbce4e0b56ba5d91eb2cd83d4584d4bd
Jay Sepkus <sepkus.jay@gmail.com>
2021-01-07 22:11:41 +0800

    init gatsby add in dotenv

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+module.exports = {
+  siteMetadata: {
+    title: "AudioCore",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
